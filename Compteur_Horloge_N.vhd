5f285a7a99c1f6f852dce537ccdd8b162be73f1c
Fabrice Wiotte <100428412+fabzz60@users.noreply.github.com>
2022-03-07 15:48:13 +0100

    Update Compteur_Horloge_N.vhd

--- Compteur_Horloge_N.vhd
+++ Compteur_Horloge_N.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 use ieee.numeric_std.all;
 
-entity Compteur_pour_HM is
+entity Compteur_pour_HN is
     Port ( CLK : in  STD_LOGIC;  --12MHz
          Reset : in  STD_LOGIC;
          Full : out  STD_LOGIC;
@@ -21,10 +21,10 @@ entity Compteur_pour_HM is
          BCD_DM : out  STD_LOGIC_VECTOR (3 downto 0);
          BCD_CM : out  STD_LOGIC_VECTOR (3 downto 0));
 
-end Compteur_pour_HM;
+end Compteur_pour_HN;
 
-architecture Behavioral of Compteur_pour_HM is
-
+architecture Behavioral of Compteur_pour_HN is
+--déclaration des signaux et des variables
 signal count1: INTEGER range 0 to 100000 := 0;
 SIGNAL clock_int1: STD_LOGIC :='0';
 signal count2: INTEGER range 0 to 100000 := 0;
@@ -43,7 +43,6 @@ signal RAM_ADDR: STD_LOGIC_VECTOR (2 downto 0); -- Address to write/read RAM
 signal RAM: RAM_ARRAY :=(
    x"00",x"00",x"00",x"00",x"00",x"00"
    );
-
 signal COUNTER_U: INTEGER range 0 to 9;	
 signal COUNTER_D: INTEGER range 0 to 5;
 signal COUNTER_M: INTEGER range 0 to 9;	
@@ -55,7 +54,7 @@ signal IS_000000: STD_LOGIC;
 signal Synchro_horloge : STD_LOGIC;
 
 begin
-
+-- definition des fréquences utiles -1KHz - 1Hz par défaut. 3 divisions de fréquences à partir du 12MHz
 PROCESS
 BEGIN
 WAIT UNTIL CLK'EVENT and CLK = '1' ;
@@ -64,7 +63,7 @@ WAIT UNTIL CLK'EVENT and CLK = '1' ;
       ELSE
       count1 <= 0;
       END IF;
-		--� la moiti� du comptage on change la valeur de clock_1Hz_int (rapport cyclique = 1/2)
+		--à la moitié du comptage on change la valeur de clock_1Hz_int (rapport cyclique = 1/2)
 		IF count1 <= M1/2 THEN
       clock_int1 <= '1';
       ELSE
@@ -76,7 +75,7 @@ WAIT UNTIL CLK'EVENT and CLK = '1' ;
       ELSE
       count2 <= 0;
       END IF;
-		--� la moiti� du comptage on change la valeur  (rapport cyclique = 1/2)
+		--à la moitié du comptage on change la valeur  (rapport cyclique = 1/2)
 		IF count2 <= M2/2 THEN
       clock_int2 <= '1';
       ELSE
@@ -88,7 +87,7 @@ WAIT UNTIL CLK'EVENT and CLK = '1' ;
       ELSE
       count3 <= 0;
       END IF;
-		--� la moiti� du comptage on change la valeur  (rapport cyclique = 1/2)
+		--à la moitié du comptage on change la valeur  (rapport cyclique = 1/2)
 		IF count3 <= M3/2 THEN
       clock_int3 <= '1';
       ELSE
@@ -96,9 +95,10 @@ WAIT UNTIL CLK'EVENT and CLK = '1' ;
       end if;
 END PROCESS;
 
+
+--reglage horloge suivant état des boutons et fréquence par défaut = 1Hz
 process(clock_int1,clock_int2,clock_int3,avance_rapide,retour_rapide)
 begin
-
 if avance_rapide ='1' then
    Synchro_horloge <= clock_int1;
 	  
@@ -108,10 +108,12 @@ elsif retour_rapide ='1' then
 else
    Synchro_horloge <= clock_int3;
 end if;
-end process;  
+end process; 
 
+-- visu Leds rouges synchronisation 1Hz
 Leds_etat_1s <="1111" when clock_int3 ='1' else "0000";
 
+-- process recupération des données USB-sérial
 process(RX_DV,reset,RST_RAM)
 begin
 if reset ='1' or RST_RAM ='1' then
@@ -123,7 +125,6 @@ if reset ='1' or RST_RAM ='1' then
 	RAM(4) <=X"00";
 	RAM(5) <=X"00";
 elsif rising_edge(RX_DV) then
-
   if Rx_data_uart>= X"30" and Rx_data_uart <= X"39" then
       IF RAM_ADDR <=6 THEN
        RAM_ADDR <= RAM_ADDR + 1;
@@ -140,8 +141,9 @@ end if;
 end process;
 
 RST_RAM <='1' when Rx_data_uart = X"72" else '0'; -- r du clavier
-start_horloge <='1' when Rx_data_uart = X"0D" else '0'; -- touche entr�e start horloge.
+start_horloge <='1' when Rx_data_uart = X"0D" else '0'; -- touche entrée start horloge.
 
+--compteur décompteur pour 6 afficheurs 7 segments
 process(Synchro_horloge,Reset,RST_RAM,RAM_ADDR)
 begin
 if reset ='1' or RST_RAM ='1' then
@@ -149,14 +151,13 @@ if reset ='1' or RST_RAM ='1' then
       COUNTER_D  <= 0;
       COUNTER_M  <= 0;
       COUNTER_T  <= 0;
-		COUNTER_DM <= 0;
-		COUNTER_CM <= 0;
-		
+	  COUNTER_DM <= 0;
+	  COUNTER_CM <= 0;		
 elsif Synchro_horloge'event and Synchro_horloge ='1' then
 		   if avance_rapide ='0' and retour_rapide = '0' then
 		       if IS_235959 = '0' then 
 		         if COUNTER_U = 9 then
-	     	         COUNTER_U <= 0;
+	     	        COUNTER_U <= 0;
 	     	      if COUNTER_D = 5 then
                   COUNTER_D <= 0;
                if COUNTER_M = 9 then
@@ -283,11 +284,11 @@ end process;
 	BCD_T <= std_logic_vector(to_unsigned(COUNTER_T,4));
 	BCD_DM <= std_logic_vector(to_unsigned(COUNTER_DM,4));
 	BCD_CM <= std_logic_vector(to_unsigned(COUNTER_CM,4));
-   
+	
+ --définition des bornes de l'horloge numérique 
  IS_235959 <= '1' when (COUNTER_U = 0 and COUNTER_D = 0 and COUNTER_M = 0 and COUNTER_T = 0 and COUNTER_DM = 4 and COUNTER_CM = 2) else '0';
  IS_000000 <= '1' when (COUNTER_U = 0 and COUNTER_D = 0 and COUNTER_M = 0 and COUNTER_T = 0 and COUNTER_DM = 0 and COUNTER_CM = 0) else '0';
- 
+ --visu led lorsque le compteur atteint 23h59h59s et 00:00:00
  Full <= IS_235959; 
  Empty <= IS_000000;
 end Behavioral;
-
