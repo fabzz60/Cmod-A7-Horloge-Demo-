8c7ef3d296ed848f596b0e5531733b78ee44db97
Fabrice Wiotte <100428412+fabzz60@users.noreply.github.com>
2022-03-07 15:52:04 +0100

    Update Compteur_Horloge_N.vhd

--- Compteur_Horloge_N.vhd
+++ Compteur_Horloge_N.vhd
@@ -10,10 +10,10 @@ entity Compteur_pour_HN is
          Full : out  STD_LOGIC;
          Empty : out  STD_LOGIC;
          avance_rapide : in  STD_LOGIC;
-		 retour_rapide : in  STD_LOGIC;
-		 RX_DV : in  STD_LOGIC;
-		 Leds_etat_1s : out  STD_LOGIC_VECTOR (3 downto 0);
-		 Rx_data_Uart : in  STD_LOGIC_VECTOR (7 downto 0);
+	 retour_rapide : in  STD_LOGIC;
+	 RX_DV : in  STD_LOGIC;
+	 Leds_etat_1s : out  STD_LOGIC_VECTOR (3 downto 0);
+	 Rx_data_Uart : in  STD_LOGIC_VECTOR (7 downto 0);
          BCD_U : out  STD_LOGIC_VECTOR (3 downto 0);
          BCD_D : out  STD_LOGIC_VECTOR (3 downto 0);
          BCD_M : out  STD_LOGIC_VECTOR (3 downto 0);
@@ -63,32 +63,32 @@ WAIT UNTIL CLK'EVENT and CLK = '1' ;
       ELSE
       count1 <= 0;
       END IF;
-		--à la moitié du comptage on change la valeur de clock_1Hz_int (rapport cyclique = 1/2)
-		IF count1 <= M1/2 THEN
+      --à la moitié du comptage on change la valeur de clock_1Hz_int (rapport cyclique = 1/2)
+      IF count1 <= M1/2 THEN
       clock_int1 <= '1';
       ELSE
       clock_int1 <= '0';
       end if;
 	
-    	IF count2 <= M2-1 THEN  
+      IF count2 <= M2-1 THEN  
       count2 <= count2 + 1;
       ELSE
       count2 <= 0;
       END IF;
-		--à la moitié du comptage on change la valeur  (rapport cyclique = 1/2)
-		IF count2 <= M2/2 THEN
+      --à la moitié du comptage on change la valeur  (rapport cyclique = 1/2)
+      IF count2 <= M2/2 THEN
       clock_int2 <= '1';
       ELSE
       clock_int2 <= '0';
       end if;
 		
-		IF count3 <= M3-1 THEN  
+      IF count3 <= M3-1 THEN  
       count3 <= count3 + 1;
       ELSE
       count3 <= 0;
       END IF;
-		--à la moitié du comptage on change la valeur  (rapport cyclique = 1/2)
-		IF count3 <= M3/2 THEN
+      --à la moitié du comptage on change la valeur  (rapport cyclique = 1/2)
+      IF count3 <= M3/2 THEN
       clock_int3 <= '1';
       ELSE
       clock_int3 <= '0';
@@ -118,12 +118,12 @@ process(RX_DV,reset,RST_RAM)
 begin
 if reset ='1' or RST_RAM ='1' then
    RAM_ADDR <="000";
-	RAM(0) <=X"00";
-	RAM(1) <=X"00";
-	RAM(2) <=X"00";
-	RAM(3) <=X"00";
-	RAM(4) <=X"00";
-	RAM(5) <=X"00";
+   RAM(0) <=X"00";
+   RAM(1) <=X"00";
+   RAM(2) <=X"00";
+   RAM(3) <=X"00";
+   RAM(4) <=X"00";
+   RAM(5) <=X"00";
 elsif rising_edge(RX_DV) then
   if Rx_data_uart>= X"30" and Rx_data_uart <= X"39" then
       IF RAM_ADDR <=6 THEN
@@ -154,24 +154,24 @@ if reset ='1' or RST_RAM ='1' then
 	  COUNTER_DM <= 0;
 	  COUNTER_CM <= 0;		
 elsif Synchro_horloge'event and Synchro_horloge ='1' then
-		   if avance_rapide ='0' and retour_rapide = '0' then
-		       if IS_235959 = '0' then 
-		         if COUNTER_U = 9 then
-	     	        COUNTER_U <= 0;
-	     	      if COUNTER_D = 5 then
+	 if avance_rapide ='0' and retour_rapide = '0' then
+	    if IS_235959 = '0' then 
+	       if COUNTER_U = 9 then
+	     	  COUNTER_U <= 0;
+	       if COUNTER_D = 5 then
                   COUNTER_D <= 0;
                if COUNTER_M = 9 then
                   COUNTER_M <= 0;
                if COUNTER_T = 5 then
                   COUNTER_T <= 0;
-					if COUNTER_DM = 9 then
+	       if COUNTER_DM = 9 then
                   COUNTER_DM <= 0;
-					if COUNTER_CM = 2 then
+	       if COUNTER_CM = 2 then
                   COUNTER_CM <= 0;
-					else
+               else
                COUNTER_CM <= COUNTER_CM + 1;
                end if; 
-					else
+	       else
                COUNTER_DM <= COUNTER_DM + 1;
                end if; 
                else
@@ -187,26 +187,25 @@ elsif Synchro_horloge'event and Synchro_horloge ='1' then
                COUNTER_U <= COUNTER_U + 1;
                end if;
              end if;
-				end if;
-
-		    if  avance_rapide ='1' and retour_rapide = '0' then 
-		       if IS_235959 = '0' then 
-		         if COUNTER_U = 9 then
-	     	         COUNTER_U <= 0;
-	     	      if COUNTER_D = 5 then
+	end if;
+    if  avance_rapide ='1' and retour_rapide = '0' then 
+	  if IS_235959 = '0' then 
+	       if COUNTER_U = 9 then
+	     	  COUNTER_U <= 0;
+	       if COUNTER_D = 5 then
                   COUNTER_D <= 0;
                if COUNTER_M = 9 then
                   COUNTER_M <= 0;
                if COUNTER_T = 5 then
                   COUNTER_T <= 0;
-					if COUNTER_DM = 9 then
+	       if COUNTER_DM = 9 then
                   COUNTER_DM <= 0;
-					if COUNTER_CM = 2 then
+	       if COUNTER_CM = 2 then
                   COUNTER_CM <= 0;
-					else
+	       else
                COUNTER_CM <= COUNTER_CM + 1;
                end if; 
-					else
+	       else
                COUNTER_DM <= COUNTER_DM + 1;
                end if; 
                else
@@ -222,25 +221,25 @@ elsif Synchro_horloge'event and Synchro_horloge ='1' then
                COUNTER_U <= COUNTER_U + 1;
                end if;
              end if;
-			end if;
-	      if retour_rapide = '1' and  avance_rapide = '0' then
-             if IS_000000 = '0' then 
-		         if COUNTER_U = 0 then
-	     	         COUNTER_U <= 9;
-	     	      if COUNTER_D = 0 then
+	end if;
+      if retour_rapide = '1' and  avance_rapide = '0' then
+           if IS_000000 = '0' then 
+	       if COUNTER_U = 0 then
+	          COUNTER_U <= 9;
+	       if COUNTER_D = 0 then
                   COUNTER_D <= 5;
                if COUNTER_M = 0 then
                   COUNTER_M <= 9;
                if COUNTER_T = 0 then
                   COUNTER_T <= 5;
-					if COUNTER_DM = 0 then
+	       if COUNTER_DM = 0 then
                   COUNTER_DM <= 9;
-					if COUNTER_CM = 0 then
+	       if COUNTER_CM = 0 then
                   COUNTER_CM <= 2;
-					else
+	       else
                COUNTER_CM <= COUNTER_CM - 1;
                end if; 
-					else
+	       else
                COUNTER_DM <= COUNTER_DM - 1;
                end if; 
                else
@@ -262,18 +261,18 @@ elsif Synchro_horloge'event and Synchro_horloge ='1' then
                 COUNTER_D  <= 0;
                 COUNTER_M  <= 0;
                 COUNTER_T  <= 0;
-		        COUNTER_DM <= 0;
-		        COUNTER_CM <= 0;	
-				end if;
+		COUNTER_DM <= 0;
+		COUNTER_CM <= 0;	
+		end if;
 					 
                 if RAM_ADDR = 6 then 	
                 COUNTER_U  <= to_integer(unsigned(RAM(5)));
                 COUNTER_D  <= to_integer(unsigned(RAM(4)));
                 COUNTER_M  <= to_integer(unsigned(RAM(3)));
                 COUNTER_T  <= to_integer(unsigned(RAM(2)));
-		        COUNTER_DM <= to_integer(unsigned(RAM(1)));
-		        COUNTER_CM <= to_integer(unsigned(RAM(0)));
-				end if;
+		COUNTER_DM <= to_integer(unsigned(RAM(1)));
+		COUNTER_CM <= to_integer(unsigned(RAM(0)));
+		end if;
  
 end if;
 end process;
